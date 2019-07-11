4f5fad1328a06c953946a2040b5fed5faa7eda0b
Vincent Demeester <vdemeest@redhat.com>
2019-07-11 11:57:26 +0200

    Make `hack/update-codegen.sh` no-op for the time being
    We don't have any code in the repository yet, so let's make this
    no-op. We will need to update it as we add code and need code
    generation.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -20,30 +20,30 @@ set -o pipefail
 
 source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/library.sh
 
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
+# CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
-KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/github.com/knative/pkg 2>/dev/null || echo ../pkg)}
+# KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/github.com/knative/pkg 2>/dev/null || echo ../pkg)}
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/tektoncd/pipeline/pkg/client github.com/tektoncd/pipeline/pkg/apis \
-  pipeline:v1alpha1 \
-  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
+# # generate the code with:
+# # --output-base    because this script should also be able to run inside the vendor dir of
+# #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+# #                  instead of the $GOPATH directly. For normal projects this can be dropped.
+# ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+#   github.com/tektoncd/pipeline/pkg/client github.com/tektoncd/pipeline/pkg/apis \
+#   pipeline:v1alpha1 \
+#   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
 
-# Depends on generate-groups.sh to install bin/deepcopy-gen
-${GOPATH}/bin/deepcopy-gen \
-  -O zz_generated.deepcopy \
-  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt \
-  -i github.com/tektoncd/pipeline/pkg/apis/config
+# # Depends on generate-groups.sh to install bin/deepcopy-gen
+# ${GOPATH}/bin/deepcopy-gen \
+#   -O zz_generated.deepcopy \
+#   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt \
+#   -i github.com/tektoncd/pipeline/pkg/apis/config
 
-# Knative Injection
-${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
-  github.com/tektoncd/pipeline/pkg/client github.com/tektoncd/pipeline/pkg/apis \
-  "pipeline:v1alpha1" \
-  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
+# # Knative Injection
+# ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
+#   github.com/tektoncd/pipeline/pkg/client github.com/tektoncd/pipeline/pkg/apis \
+#   "pipeline:v1alpha1" \
+#   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
 
 # Make sure our dependencies are up-to-date
 ${REPO_ROOT_DIR}/hack/update-deps.sh
