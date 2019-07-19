c9d97745524e51d8a962f7aaa2354f0f14cce2fb
Sunil Thaha <sthaha@redhat.com>
2019-07-19 20:17:15 +1000

    Initial port of OpenShift/pipelines-operator
    This is the initial version of the tekton operator that installs
    0.5.2 version of tekton pipelines and is based on the
    openshift-pipeline-operator:
    see: https://github.com/openshift/tektoncd-pipeline-operator/commit/039bd4c5d2dd31557a9f34171299c50b3f80172d
    
    The operator automatically installs pipelines when started by
    - creating a `config.operator.tekton.dev` CR with the name `cluster`
    - Once the cluster `cr` is created, it installs tekton-pipelines and
      updates the status of the `cluster` CR to indicate the status of
      installation of pipelines.
    
    Signed-off-by: sthaha@redhat.com

---
+++ pkg/apis/operator/group.go
@@ -0,0 +1,6 @@
+// Package operator contains operator API versions.
+//
+// This file ensures Go source parsers acknowledge the operator package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package operator
---
+++ test/helpers/helpers.go
@@ -0,0 +1,124 @@
+package helpers
+
+import (
+	"context"
+	"testing"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	"github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	op "github.com/tektoncd/operator/pkg/apis/operator/v1alpha1"
+	"github.com/tektoncd/operator/test/config"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// AssertNoError confirms the error returned is nil
+func AssertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// WaitForDeploymentDeletion checks to see if a given deployment is deleted
+// the function returns an error if the given deployment is not deleted within the timeout
+func WaitForDeploymentDeletion(t *testing.T, namespace, name string) error {
+	t.Helper()
+
+	err := wait.Poll(config.APIRetry, config.APITimeout, func() (bool, error) {
+		kc := test.Global.KubeClient
+		_, err := kc.AppsV1().Deployments(namespace).Get(name, metav1.GetOptions{IncludeUninitialized: true})
+		if err != nil {
+			if apierrors.IsGone(err) || apierrors.IsNotFound(err) {
+				return true, nil
+			}
+			return false, err
+		}
+
+		t.Logf("Waiting for deletion of %s deployment\n", name)
+		return false, nil
+	})
+	if err == nil {
+		t.Logf("%s Deployment deleted\n", name)
+	}
+	return err
+}
+
+func WaitForClusterCR(t *testing.T, name string) *op.Config {
+	t.Helper()
+
+	objKey := types.NamespacedName{Name: name}
+	cr := &op.Config{}
+
+	err := wait.Poll(config.APIRetry, config.APITimeout, func() (bool, error) {
+		err := test.Global.Client.Get(context.TODO(), objKey, cr)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				t.Logf("Waiting for availability of %s cr\n", name)
+				return false, nil
+			}
+			return false, err
+		}
+
+		return true, nil
+	})
+
+	AssertNoError(t, err)
+	return cr
+}
+
+func DeleteClusterCR(t *testing.T, name string) {
+	t.Helper()
+
+	// ensure object exists before deletion
+	objKey := types.NamespacedName{Name: name}
+	cr := &op.Config{}
+	err := test.Global.Client.Get(context.TODO(), objKey, cr)
+	if err != nil {
+		t.Logf("Failed to find cluster CR: %s : %s\n", name, err)
+	}
+	AssertNoError(t, err)
+
+	err = wait.Poll(config.APIRetry, config.APITimeout, func() (bool, error) {
+		err := test.Global.Client.Delete(context.TODO(), cr)
+		if err != nil {
+			t.Logf("Deletion of CR %s failed %s \n", name, err)
+			return false, err
+		}
+
+		return true, nil
+	})
+
+	AssertNoError(t, err)
+}
+
+func ValidatePipelineSetup(t *testing.T, cr *op.Config, deployments ...string) {
+	t.Helper()
+
+	kc := test.Global.KubeClient
+	ns := cr.Spec.TargetNamespace
+
+	for _, d := range deployments {
+		err := e2eutil.WaitForDeployment(
+			t, kc, ns,
+			d,
+			1,
+			config.APIRetry,
+			config.APITimeout,
+		)
+		AssertNoError(t, err)
+	}
+}
+
+func ValidatePipelineCleanup(t *testing.T, cr *op.Config, deployments ...string) {
+	t.Helper()
+
+	ns := cr.Spec.TargetNamespace
+	for _, d := range deployments {
+		err := WaitForDeploymentDeletion(t, ns, d)
+		AssertNoError(t, err)
+	}
+}
