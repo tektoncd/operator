eafb70c6c772fad6c50449cf2cb31f941a85ffbe
Nikhil Thomas <nikthoma@redhat.com>
2019-08-07 16:45:02 +0530

    Add new CRD addon.operator.tekton.dev
    Add new CRd addon.operator.tekton.dev and related file generated by
    ```
    operator-sdk add api --api-version operator.tekton.dev/v1alpha1 --kind Addon
    ```
    
    Add a new CRD definition in deploy/crd
    
    Update deploy/role.yaml

---
+++ pkg/apis/operator/v1alpha1/addon_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AddonSpec defines the desired state of Addon
+// +k8s:openapi-gen=true
+type AddonSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// AddonStatus defines the observed state of Addon
+// +k8s:openapi-gen=true
+type AddonStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Addon is the Schema for the addons API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type Addon struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AddonSpec   `json:"spec,omitempty"`
+	Status AddonStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AddonList contains a list of Addon
+type AddonList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Addon `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Addon{}, &AddonList{})
+}
