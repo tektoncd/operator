9e5c4f6b5baa29b7dfe1a093bc968a505b921e6d
Vincent Hou <shou@us.ibm.com>
2019-08-14 14:28:33 -0400

    Add dependencies under vendor 🧗‍♂️

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -14,30 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script calls out to scripts in tektoncd/plumbing to setup a cluster
-# and deploy Tekton Pipelines to it for running integration tests.
-declare -r SCRIPT_PATH=$(readlink -f "$0")
-declare -r SCRIPT_DIR=$(cd $(dirname "$SCRIPT_PATH") && pwd)
-
-# ensure the current working dir is the root of the project
-cd $SCRIPT_DIR/../
-
-# This script needs helper functions from tektoncd/plumbing/scripts and
-# although github.com/tektoncd/plumbing is added as a go mod dependency,
-# the package may not exists when the test is running, so, it ensure the
-# package is available, run go mod download which downloads the packages to
-# $GOPATH/pkg/mod/repo/path@version
-# GOPROXY ensures the downloads is faster
-
 export GO111MODULE=on
-export GOPROXY="https://proxy.golang.org"
-go mod download
-
-plumbing_dir_name="$(grep github.com/tektoncd/plumbing go.sum |
-  grep -v go.mod |
-  head -1 | awk '{ print $1 "@" $2 }')"
-plumbing_path="$(go env GOPATH)/pkg/mod/$plumbing_dir_name"
-source "$plumbing_path/scripts/e2e-tests.sh"
+source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/e2e-tests.sh
 
 # Script entry point.
 
