1be3871acde65a98eb78e6f8717fe2f425bb3de0
Vincent Hou <shou@us.ibm.com>
2020-06-16 09:51:39 -0400

    Update the script hack/update-deps.sh

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2019 The Tekton Authors
+# Copyright 2020 The Knative Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,15 +14,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+readonly ROOT_DIR=$(dirname $0)/..
+source ${ROOT_DIR}/vendor/github.com/tektoncd/plumbing/scripts/library.sh
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
-source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/library.sh
+cd ${ROOT_DIR}
 
-cd ${REPO_ROOT_DIR}
+# We need these flags for things to work properly.
+export GO111MODULE=on
 
-# Ensure we have everything we need under vendor/
-dep ensure
+# This controls the release branch we track.
+VERSION="master"
 
-update_licenses third_party/VENDOR-LICENSE "./cmd/*"
+# The list of dependencies that we track at HEAD and periodically
+# float forward in this repository.
+FLOATING_DEPS=(
+)
+
+# Parse flags to determine any we should pass to dep.
+GO_GET=0
+while [[ $# -ne 0 ]]; do
+  parameter=$1
+  case ${parameter} in
+    --upgrade) GO_GET=1 ;;
+    *) abort "unknown option ${parameter}" ;;
+  esac
+  shift
+done
+readonly GO_GET
+
+if (( GO_GET )); then
+  go get -d ${FLOATING_DEPS[@]}
+fi
+
+# Prune modules.
+go mod tidy
+go mod vendor
+
+rm -rf $(find vendor/ -name 'OWNERS')
+rm -rf $(find vendor/ -name '*_test.go')
+
+export GOFLAGS=-mod=vendor
+
+update_licenses third_party/VENDOR-LICENSE "./..."
+
+remove_broken_symlinks ./vendor
