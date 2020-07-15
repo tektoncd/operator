f29f4e77b06beb17ebb37de14d45618bfb33f82b
Vincent Hou <shou@us.ibm.com>
2020-07-15 14:29:30 -0400

    Update the API folder and CRD to accommodate knative pkg

--- pkg/apis/operator/v1alpha1/register.go
+++ pkg/apis/operator/v1alpha1/register.go
@@ -6,14 +6,41 @@
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
+
+const (
+	// GroupName is the group of the API.
+	GroupName = "operator.tekton.dev"
+
+	// SchemaVersion is the current version of the API.
+	SchemaVersion = "v1alpha1"
+)
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// addKnownTypes adds the set of types defined in this package to the supplied
+// scheme.
+func addKnownTypes(s *runtime.Scheme) error {
+	s.AddKnownTypes(SchemeGroupVersion,
+		&TektonPipeline{},
+		&TektonPipelineList{},
+		&TektonAddon{},
+		&TektonAddonList{})
+	metav1.AddToGroupVersion(s, SchemeGroupVersion)
+	return nil
+}
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "operator.tekton.dev", Version: "v1alpha1"}
-
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: SchemaVersion}
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	// AddToScheme adds the API's types to the Scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
 )
