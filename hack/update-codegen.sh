c8c9aebc8f6c788aba516f89bd0c49f7441b055d
Vincent Demeester <vdemeest@redhat.com>
2020-11-12 16:20:31 +0100

    Update codegen code
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2020 The Tekton Authors
+# Copyright 2019 The Tekton Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,37 +18,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export GO111MODULE=on
-
-if [ -z "${GOPATH:-}" ]; then
-  export GOPATH=$(go env GOPATH)
-fi
-
 source $(git rev-parse --show-toplevel)/vendor/github.com/tektoncd/plumbing/scripts/library.sh
 
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+PREFIX=${GOBIN:-${GOPATH}/bin}
 
-KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/knative.dev/pkg 2>/dev/null || echo ../pkg)}
-
+OLDGOFLAGS="${GOFLAGS:-}"
+GOFLAGS="-mod=vendor"
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/tektoncd/operator/pkg/client github.com/tektoncd/operator/pkg/apis \
-  "operator:v1alpha1" \
-  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
-
-bash ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
+bash ${REPO_ROOT_DIR}/hack/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/tektoncd/operator/pkg/client github.com/tektoncd/operator/pkg/apis \
   "operator:v1alpha1" \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
 
 # Depends on generate-groups.sh to install bin/deepcopy-gen
-${GOPATH}/bin/deepcopy-gen \
+${PREFIX}/deepcopy-gen \
   -O zz_generated.deepcopy \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt \
   -i github.com/tektoncd/operator/pkg/apis/operator/v1alpha1
 
 # Make sure our dependencies are up-to-date
 ${REPO_ROOT_DIR}/hack/update-deps.sh
+
+# Make sure the OpenAPI specification and Swagger file are up-to-date
+${REPO_ROOT_DIR}/hack/update-openapigen.sh
