5fe91fdfd78ee98647cc96c6035c6f201ebae2b9
Nikhil Thomas <nikthoma@redhat.com>
2021-08-10 15:57:05 +0530

    Update release pipelines by adding payload fetch Task
    Add `operator-fetch-component-releases` Task to run the
    `/hack/fetch-releases.sh` script.
    
    Update release pipeline by adding the payload fetch Task
    
    Add Dashboard and Results support to payload fetch Task
    
    Signed-off-by: Nikhil Thomas <nikthoma@redhat.com>

--- pkg/reconciler/common/releases.go
+++ pkg/reconciler/common/releases.go
@@ -88,7 +88,7 @@ func ComponentDir(instance v1alpha1.TektonComponent) string {
 	case *v1alpha1.TektonConfig:
 		return filepath.Join(koDataDir, "tekton-config")
 	case *v1alpha1.TektonResult:
-		return filepath.Join(koDataDir, "tekton-result")
+		return filepath.Join(koDataDir, "tekton-results")
 	}
 	return ""
 }
