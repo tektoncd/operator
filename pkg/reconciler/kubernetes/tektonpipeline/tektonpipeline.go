0afc6d6c5321dd6617fd396047160d96ff98141d
Shivam Mukhade <smukhade@redhat.com>
2021-11-16 17:58:28 +0530

    Move update spec calls to the end of reconciler
    This would resolve some of the flakiness used to occur
    while installing Operator. Because of updating the object
    in the middle of reconciler, sometime used to get error
    ` the object has been modified; please apply your changes to
    the latest version and try again `
    so this moves the update spec call to the end of reconciler.
    
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- pkg/reconciler/kubernetes/tektonpipeline/tektonpipeline.go
+++ pkg/reconciler/kubernetes/tektonpipeline/tektonpipeline.go
@@ -31,6 +31,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/pkg/apis"
 	"knative.dev/pkg/logging"
 	pkgreconciler "knative.dev/pkg/reconciler"
@@ -261,6 +262,11 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, tp *v1alpha1.TektonPipel
 
 	// Mark PostReconcile Complete
 	tp.Status.MarkPostReconcilerComplete()
+
+	// Update the object for any spec changes
+	if _, err := r.operatorClientSet.OperatorV1alpha1().TektonPipelines().Update(ctx, tp, v1.UpdateOptions{}); err != nil {
+		return err
+	}
 
 	return nil
 }
