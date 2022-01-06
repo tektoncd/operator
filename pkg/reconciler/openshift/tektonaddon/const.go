99edd78ffc995c56855f44f96f02f034ec7cd4b6
savitaashture <sashture@redhat.com>
2022-01-06 21:02:09 +0530

    Fix multiple installersets for tektonaddon

--- pkg/reconciler/openshift/tektonaddon/const.go
+++ pkg/reconciler/openshift/tektonaddon/const.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Tekton Authors
+Copyright 2022 The Tekton Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,12 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tektoninstallerset
+package tektonaddon
 
 const (
-	LastAppliedHashKey = "operator.tekton.dev/last-applied-hash"
-	CreatedByKey       = "operator.tekton.dev/created-by"
-	ReleaseVersionKey  = "operator.tekton.dev/release-version"
-	TargetNamespaceKey = "operator.tekton.dev/target-namespace"
-	InstallerSetType   = "operator.tekton.dev/type"
+	ClusterTaskInstallerSet            = "ClusterTask"
+	PipelinesTemplateInstallerSet      = "PipelinesTemplate"
+	TriggersResourcesInstallerSet      = "TriggersResources"
+	ConsoleCLIInstallerSet             = "ConsoleCLI"
+	MiscellaneousResourcesInstallerSet = "MiscellaneousResources"
+	CreatedByValue                     = "TektonAddon"
 )
