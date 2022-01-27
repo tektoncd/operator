62d6f857472fcb8b0b54160a519a74992b1ab408
Shivam Mukhade <smukhade@redhat.com>
2022-01-27 16:55:37 +0530

    [OpenShift] fixes post pipelines installerSet creation
    post pipeline is not getting created on OpenShift, this
    fixes it.
    
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>
    (cherry picked from commit 959795423e9fd19d6d13c66514d3926fe7bfb882)

--- pkg/reconciler/openshift/tektonpipeline/extension.go
+++ pkg/reconciler/openshift/tektonpipeline/extension.go
@@ -57,7 +57,7 @@ var (
 		tektoninstallerset.CreatedByKey, createdByValue,
 	)
 	postReconcileSelector = fmt.Sprintf("%s=%s,%s=%s",
-		tektoninstallerset.InstallerSetType, prePipelineInstallerSet,
+		tektoninstallerset.InstallerSetType, postPipelineInstallerSet,
 		tektoninstallerset.CreatedByKey, createdByValue,
 	)
 )
@@ -192,12 +192,12 @@ func (oe openshiftExtension) PostReconcile(ctx context.Context, comp v1alpha1.Te
 }
 func (oe openshiftExtension) Finalize(ctx context.Context, comp v1alpha1.TektonComponent) error {
 	pipeline := comp.(*v1alpha1.TektonPipeline)
-	err := deleteInstallerSet(ctx, oe.operatorClientSet, pipeline, postPipelineInstallerSet, postReconcileSelector)
-	if err != nil {
+	if err := deleteInstallerSet(ctx, oe.operatorClientSet, pipeline,
+		postPipelineInstallerSet, postReconcileSelector); err != nil {
 		return err
 	}
-	err = deleteInstallerSet(ctx, oe.operatorClientSet, pipeline, postPipelineInstallerSet, postReconcileSelector)
-	if err != nil {
+	if err := deleteInstallerSet(ctx, oe.operatorClientSet, pipeline,
+		prePipelineInstallerSet, preReconcileSelector); err != nil {
 		return err
 	}
 	return nil
