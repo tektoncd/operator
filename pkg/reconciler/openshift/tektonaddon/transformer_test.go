5487942c4890535dbf7c4e9031220624966e6143
Shubham <concaf@redhat.com>
2022-03-09 16:14:55 +0530

    Replace PAC images in TriggerTemplates
    Before this commit, the images in TriggerTemplates deployed by Pipelines
    as Code are not substituted with images required for OperatorHub
    deployment.
    
    This commit adds a transformer that replaces images per the environment
    variable `IMAGE_PAC_TRIGGERTEMPLATE_<STEP_NAME>`.

--- pkg/reconciler/openshift/tektonaddon/transformer_test.go
+++ pkg/reconciler/openshift/tektonaddon/transformer_test.go
@@ -17,6 +17,11 @@ limitations under the License.
 package tektonaddon
 
 import (
+	"bytes"
+	"encoding/json"
+	pipelinev1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	triggersv1beta1 "github.com/tektoncd/triggers/pkg/apis/triggers/v1beta1"
+	"os"
 	"path"
 	"testing"
 
@@ -72,5 +77,46 @@ func TestSetVersionedNames(t *testing.T) {
 
 	if d := cmp.Diff(expectedManifest.Resources(), newManifest.Resources()); d != "" {
 		t.Errorf("failed to update versioned clustertask name %s", diff.PrintWantGot(d))
+	}
+}
+
+func TestReplacePACTriggerTemplateImages(t *testing.T) {
+	testData := path.Join("testdata", "test-triggertemplate-taskrun.yaml")
+	manifest, err := mf.ManifestFrom(mf.Recursive(testData))
+	assert.NilError(t, err)
+
+	stepName := "apply-and-launch"
+	pacTestImage := "registry.test.io/openshift-pipelines/pipelines-as-code"
+	err = os.Setenv("IMAGE_PAC_TRIGGERTEMPLATE_APPLY_AND_LAUNCH", pacTestImage)
+	assert.NilError(t, err)
+
+	pacImages := pacTriggerTemplateStepImages()
+	t.Log(pacImages)
+	assert.Equal(t, len(pacImages), 1)
+
+	transformedManifest, err := manifest.Transform(replacePACTriggerTemplateImages(pacImages))
+	assert.NilError(t, err)
+
+	for _, resource := range transformedManifest.Resources() {
+		// replacement is only done for TriggerTemplates
+		if resource.GetKind() != "TriggerTemplate" {
+			continue
+		}
+		tt := &triggersv1beta1.TriggerTemplate{}
+		err := runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, tt)
+		assert.NilError(t, err)
+
+		for _, resourceTemplate := range tt.Spec.ResourceTemplates {
+			taskRun := pipelinev1beta1.TaskRun{}
+			decoder := json.NewDecoder(bytes.NewBuffer(resourceTemplate.RawExtension.Raw))
+			err := decoder.Decode(&taskRun)
+			assert.NilError(t, err)
+
+			for _, step := range taskRun.Spec.TaskSpec.Steps {
+				if step.Name == stepName {
+					assert.Equal(t, step.Container.Image, pacTestImage)
+				}
+			}
+		}
 	}
 }
