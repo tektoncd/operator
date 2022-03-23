df1a8256caf1605071597326e9d270725d5d9703
Piyush Garg <pgarg@redhat.com>
2022-03-23 17:03:51 +0530

    Add operator info configmap rbac
    This will add the rbac permissions for operator-info configmap
    so that all authenticated users can get version info
    
    Remove some redudant code and kodata

--- pkg/reconciler/common/targetnamespace_test.go
+++ pkg/reconciler/common/targetnamespace_test.go
@@ -18,52 +18,13 @@ package common
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	mf "github.com/manifestival/manifestival"
 	"github.com/tektoncd/operator/pkg/apis/operator/v1alpha1"
 	"gotest.tools/v3/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/kubernetes/fake"
 )
-
-type fakeClient struct {
-	err            error
-	getErr         error
-	createErr      error
-	resourcesExist bool
-	creates        []unstructured.Unstructured
-	deletes        []unstructured.Unstructured
-}
-
-var configMap = namespacedResource("v1", "ConfigMap", "test-ns", "operators-info")
-
-func (f *fakeClient) Get(obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
-	var resource *unstructured.Unstructured
-	if f.resourcesExist {
-		resource = &unstructured.Unstructured{}
-	}
-	return resource, f.getErr
-}
-
-func (f *fakeClient) Create(obj *unstructured.Unstructured, options ...mf.ApplyOption) error {
-	obj.SetAnnotations(nil) // Deleting the extra annotation. Irrelevant for the test.
-	f.creates = append(f.creates, *obj)
-	return f.createErr
-}
-
-func (f *fakeClient) Delete(obj *unstructured.Unstructured, options ...mf.DeleteOption) error {
-	f.deletes = append(f.deletes, *obj)
-	return f.err
-}
-
-func (f *fakeClient) Update(obj *unstructured.Unstructured, options ...mf.ApplyOption) error {
-	return f.err
-}
 
 func TestCreateTargetNamespace(t *testing.T) {
 	targetNamespace := "test-ns"
@@ -103,40 +64,4 @@ func TestTargetNamespaceNotFound(t *testing.T) {
 
 	_, err = fakeClientset.CoreV1().Namespaces().Get(context.Background(), "foo", metav1.GetOptions{})
 	assert.Equal(t, err.Error(), `namespaces "foo" not found`)
-}
-
-func TestOperatorVersionCreateConfigMap(t *testing.T) {
-	os.Setenv(KoEnvKey, "testdata/kodata")
-	defer os.Unsetenv(KoEnvKey)
-
-	targetNamespace := "test-ns"
-	component := &v1alpha1.TektonPipeline{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-name",
-		},
-		Spec: v1alpha1.TektonPipelineSpec{
-			CommonSpec: v1alpha1.CommonSpec{
-				TargetNamespace: targetNamespace,
-			},
-		},
-	}
-	fakeClientset := fake.NewSimpleClientset()
-
-	err := CreateTargetNamespace(context.Background(), nil, component, fakeClientset)
-	assert.Equal(t, err, nil)
-
-	var manifest mf.Manifest
-
-	client := &fakeClient{}
-	manifest, err = mf.ManifestFrom(mf.Slice(manifest.Resources()), mf.UseClient(client))
-	assert.Equal(t, err, nil)
-
-	err = CreateOperatorVersionConfigMap(manifest, component)
-	assert.Equal(t, err, nil)
-
-	want := []unstructured.Unstructured{configMap}
-
-	if len(want) != len(client.creates) {
-		t.Fatalf("Unexpected creates: %s", fmt.Sprintf("(-got, +want): %s", cmp.Diff(client.creates, want)))
-	}
 }
