4923270ebd2e553e4e290ee35bdab26e1206a25f
Vincent Demeester <vdemeest@redhat.com>
2022-07-27 16:12:38 +0200

    Add a bump (and bump-bugfix) command to operator-tool
    The idea is similar to the check subcommand, but it edits the
    components.yaml with the latest found version (either bugfix or "in
    general").
    
    With this subcommand, we can have a cronjob and a "bot" that
    automatically updates the versions of the main components.yaml
    file. And we could have a similar one for the release branches.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- cmd/operator-tool/check.go
+++ cmd/operator-tool/check.go
@@ -44,32 +44,59 @@ func check(filename string, bugfix bool) error {
 }
 
 func checkComponent(ctx context.Context, name string, component component, bugfix bool) error {
-	client, err := gh.RESTClient(nil)
+	newerVersion, err := checkComponentNewerVersions(component, bugfix)
 	if err != nil {
 		return err
+	}
+	if len(newerVersion) > 0 {
+		fmt.Printf("%s: %v\n", name, newerVersion)
+	}
+
+	return nil
+}
+
+func checkComponentNewerVersions(component component, bugfix bool) (semver.Collection, error) {
+	sVersions, err := fetchVersions(component.Github)
+	if err != nil {
+		return nil, err
+	}
+	currentVersion, err := semver.NewVersion(component.Version)
+	if err != nil {
+		return nil, err
+	}
+	newerVersion, err := getNewerVersion(currentVersion, sVersions, bugfix)
+	if err != nil {
+		return nil, err
+	}
+	return newerVersion, nil
+}
+
+func fetchVersions(github string) (semver.Collection, error) {
+	client, err := gh.RESTClient(nil)
+	if err != nil {
+		return nil, err
 	}
 	versions := []struct {
 		Name    string
 		TagName string `json:"tag_name"`
 	}{}
-	err = client.Get(fmt.Sprintf("repos/%s/releases", component.Github), &versions)
+	err = client.Get(fmt.Sprintf("repos/%s/releases", github), &versions)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	sVersions := semver.Collection([]*semver.Version{})
 	for _, v := range versions {
 		sVersion, err := semver.NewVersion(v.TagName)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		sVersions = append(sVersions, sVersion)
 	}
 	sort.Sort(sVersions)
+	return sVersions, nil
+}
 
-	currentVersion, err := semver.NewVersion(component.Version)
-	if err != nil {
-		return err
-	}
+func getNewerVersion(currentVersion *semver.Version, versions []*semver.Version, bugfix bool) (semver.Collection, error) {
 	constraint := fmt.Sprintf("> %s", currentVersion)
 	if bugfix {
 		nextMinorVersion := currentVersion.IncMinor()
@@ -77,17 +104,13 @@ func checkComponent(ctx context.Context, name string, component component, bugfi
 	}
 	c, err := semver.NewConstraint(constraint)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	newerVersion := []*semver.Version{}
-	for _, sv := range sVersions {
+	newerVersion := semver.Collection([]*semver.Version{})
+	for _, sv := range versions {
 		if c.Check(sv) {
 			newerVersion = append(newerVersion, sv)
 		}
 	}
-	if len(newerVersion) > 0 {
-		fmt.Printf("%s: %v\n", name, newerVersion)
-	}
-
-	return nil
+	return newerVersion, nil
 }
