90b0b6f21aac10aca6a87be1e942ef8782c38ddf
Shivam Mukhade <smukhade@redhat.com>
2022-11-02 10:28:36 +0530

    Removes obosolete installer sets on upgrade
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- third_party/github.com/letsencrypt/boulder/sa/proto/sa.pb.go
+++ third_party/github.com/letsencrypt/boulder/sa/proto/sa.pb.go
@@ -7,13 +7,14 @@
 package proto
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	proto "github.com/letsencrypt/boulder/core/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
