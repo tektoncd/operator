6a723b3a700aad7f00e0642b4a0f781478620d3f
Piyush Garg <pgarg@redhat.com>
2023-09-26 16:52:21 +0530

    Bump tkn to 0.32.0

--- pkg/reconciler/openshift/tektonaddon/consolecli.go
+++ pkg/reconciler/openshift/tektonaddon/consolecli.go
@@ -66,7 +66,7 @@ func getRouteHost(manifest *mf.Manifest) (string, error) {
 }
 
 func consoleCLITransform(ctx context.Context, manifest *mf.Manifest, baseURL string) error {
-	tknVersion := "0.31.1"
+	tknVersion := "0.32.0"
 
 	if baseURL == "" {
 		return fmt.Errorf("route url should not be empty")
