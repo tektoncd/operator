9915797acc8ab2ffe61df7aeb0232d22ba8dc74a
Khurram Baig <kbaig@redhat.com>
2024-05-09 07:00:20 +0530

    Bound SA Token to Tekton Result API server in OpenShift
    This enables workload identity federation.

--- pkg/reconciler/openshift/tektonresult/extension.go
+++ pkg/reconciler/openshift/tektonresult/extension.go
@@ -27,6 +27,10 @@ import (
 	"github.com/tektoncd/operator/pkg/reconciler/common"
 	"github.com/tektoncd/operator/pkg/reconciler/kubernetes/tektoninstallerset/client"
 	occommon "github.com/tektoncd/operator/pkg/reconciler/openshift/common"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 	"knative.dev/pkg/logging"
 )
 
@@ -34,6 +38,10 @@ const (
 	// manifests console plugin yaml directory location
 	routeRBACYamlDirectory  = "static/tekton-results/route-rbac"
 	internalDBYamlDirectory = "static/tekton-results/internal-db"
+	deploymentAPI           = "tekton-results-api"
+	apiContainerName        = "api"
+	boundSAVolume           = "bound-sa-token"
+	boundSAPath             = "/var/run/secrets/openshift/serviceaccount"
 )
 
 func OpenShiftExtension(ctx context.Context) common.Extension {
@@ -72,10 +80,13 @@ type openshiftExtension struct {
 }
 
 func (oe openshiftExtension) Transformers(comp v1alpha1.TektonComponent) []mf.Transformer {
+	instance := comp.(*v1alpha1.TektonResult)
+
 	return []mf.Transformer{
 		occommon.RemoveRunAsUser(),
 		occommon.RemoveRunAsGroup(),
 		occommon.ApplyCABundles,
+		injectBoundSAToken(instance.Spec.ResultsAPIProperties),
 	}
 }
 
@@ -139,5 +150,79 @@ func filterAndTransform() client.FilterAndTransform {
 			return nil, err
 		}
 		return manifest, nil
+	}
+}
+
+// injectBoundSAToken adds a sa token projected volume to the Results Deployment
+func injectBoundSAToken(props v1alpha1.ResultsAPIProperties) mf.Transformer {
+	return func(u *unstructured.Unstructured) error {
+		if !props.LogsAPI ||
+			u.GetKind() != "Deployment" || u.GetName() != deploymentAPI {
+			return nil
+		}
+
+		d := &appsv1.Deployment{}
+		err := k8sruntime.DefaultUnstructuredConverter.FromUnstructured(u.Object, d)
+		if err != nil {
+			return err
+		}
+
+		// find the matching container and add env and secret name object
+		for i, container := range d.Spec.Template.Spec.Containers {
+			if container.Name != apiContainerName {
+				continue
+			}
+			add := true
+			vol := corev1.Volume{
+				Name: boundSAVolume,
+				VolumeSource: corev1.VolumeSource{
+					Projected: &corev1.ProjectedVolumeSource{
+						Sources: []corev1.VolumeProjection{
+							{
+								ServiceAccountToken: &corev1.ServiceAccountTokenProjection{
+									Audience: "openshift",
+									Path:     "token",
+								},
+							},
+						},
+					},
+				},
+			}
+			for k := 0; k < len(d.Spec.Template.Spec.Volumes); k++ {
+				if d.Spec.Template.Spec.Volumes[k].Name == boundSAVolume {
+					d.Spec.Template.Spec.Volumes[k] = vol
+					add = false
+				}
+			}
+			if add {
+				d.Spec.Template.Spec.Volumes = append(d.Spec.Template.Spec.Volumes, vol)
+			}
+
+			volMount := corev1.VolumeMount{
+				Name:      boundSAVolume,
+				MountPath: boundSAPath,
+			}
+
+			add = true
+			for k := 0; k < len(d.Spec.Template.Spec.Containers[i].VolumeMounts); k++ {
+				if d.Spec.Template.Spec.Containers[i].VolumeMounts[k].Name == boundSAVolume {
+					d.Spec.Template.Spec.Containers[i].VolumeMounts[k] = volMount
+					add = false
+				}
+			}
+			if add {
+				d.Spec.Template.Spec.Containers[i].VolumeMounts = append(
+					d.Spec.Template.Spec.Containers[i].VolumeMounts, volMount)
+			}
+
+			break
+		}
+
+		uObj, err := k8sruntime.DefaultUnstructuredConverter.ToUnstructured(d)
+		if err != nil {
+			return err
+		}
+		u.SetUnstructuredContent(uObj)
+		return nil
 	}
 }
