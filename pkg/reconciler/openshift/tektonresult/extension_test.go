893ae13e73580811e6164df71d002808d93cdc2d
Khurram Baig <kbaig@redhat.com>
2024-05-09 07:00:20 +0530

    Bound SA Token to Tekton Result API server in OpenShift
    This enables workload identity federation.

--- pkg/reconciler/openshift/tektonresult/extension_test.go
+++ pkg/reconciler/openshift/tektonresult/extension_test.go
@@ -18,9 +18,14 @@ package tektonresult
 
 import (
 	"os"
+	"path"
 	"testing"
 
+	mf "github.com/manifestival/manifestival"
+	"github.com/tektoncd/operator/pkg/apis/operator/v1alpha1"
 	"github.com/tektoncd/operator/pkg/reconciler/common"
+	"gotest.tools/v3/assert"
+	appsv1 "k8s.io/api/apps/v1"
 	rbac "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -48,4 +53,29 @@ func assertNoEror(t *testing.T, err error) {
 	if err != nil {
 		t.Errorf("assertion failed; expected no error %v", err)
 	}
+}
+
+func Test_injecBoundSAToken(t *testing.T) {
+	testData := path.Join("testdata", "api-deployment.yaml")
+	manifest, err := mf.ManifestFrom(mf.Recursive(testData))
+	assert.NilError(t, err)
+
+	deployment := &appsv1.Deployment{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(manifest.Resources()[0].Object, deployment)
+	assert.NilError(t, err)
+	props := v1alpha1.ResultsAPIProperties{
+		LogsAPI:        true,
+		LogsType:       "File",
+		LogsPath:       "logs",
+		LoggingPVCName: "tekton-logs",
+	}
+
+	manifest, err = manifest.Transform(injectBoundSAToken(props))
+	assert.NilError(t, err)
+
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(manifest.Resources()[0].Object, deployment)
+	assert.NilError(t, err)
+
+	assert.Equal(t, deployment.Spec.Template.Spec.Volumes[2].Name, "bound-sa-token")
+	assert.Equal(t, deployment.Spec.Template.Spec.Containers[0].VolumeMounts[2].Name, "bound-sa-token")
 }
