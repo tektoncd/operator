a146bd96bb7e71cc8d242161eb6d8b53a63e57a6
Jeeva Kandasamy <jkandasa@redhat.com>
2024-05-14 11:19:28 +0530

    update minor version and sync images
    Signed-off-by: Jeeva Kandasamy <jkandasa@redhat.com>

--- pkg/reconciler/openshift/tektonaddon/consolecli.go
+++ pkg/reconciler/openshift/tektonaddon/consolecli.go
@@ -66,7 +66,7 @@ func getRouteHost(manifest *mf.Manifest) (string, error) {
 }
 
 func consoleCLITransform(ctx context.Context, manifest *mf.Manifest, baseURL string) error {
-	tknVersion := "0.33.0"
+	tknVersion := "0.37.0"
 
 	if baseURL == "" {
 		return fmt.Errorf("route url should not be empty")
