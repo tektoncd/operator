b02dd1926dab6d3be4c15afcaf7547d6591e8b91
Piyush Garg <pgarg@redhat.com>
2024-06-13 01:36:17 +0530

    Fix validation for additionalPacController settings
    This will fix the panic error on additionalPacController if
    provided with no settings, as we dont need to set all the fields
    here of settings in additional PAC controller, we dont need to
    perform defaulting, we just need to perform validations on the
    provided fields if any
    
    also added test for the scenario

--- pkg/apis/operator/v1alpha1/openshiftpipelinesascode_validation_test.go
+++ pkg/apis/operator/v1alpha1/openshiftpipelinesascode_validation_test.go
@@ -25,6 +25,31 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func TestValidateAddtionalPACControllerEmptySettings(t *testing.T) {
+	opacCR := &OpenShiftPipelinesAsCode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "name",
+			Namespace: "namespace",
+		},
+		Spec: OpenShiftPipelinesAsCodeSpec{
+			CommonSpec: CommonSpec{
+				TargetNamespace: "openshift-pipelines",
+			},
+			PACSettings: PACSettings{
+				Settings: map[string]string{},
+				AdditionalPACControllers: map[string]AdditionalPACControllerConfig{
+					"test": {
+						ConfigMapName: "test-configmap",
+						SecretName:    "test-secret",
+					},
+				},
+			},
+		},
+	}
+	err := opacCR.Validate(context.TODO())
+	assert.Error(t, err, "")
+}
+
 func TestValidateAddtionalPACControllerInvalidName(t *testing.T) {
 	opacCR := &OpenShiftPipelinesAsCode{
 		ObjectMeta: metav1.ObjectMeta{
@@ -107,4 +132,33 @@ func TestValidateAddtionalPACControllerInvalidNameLength(t *testing.T) {
 	}
 	err := opacCR.Validate(context.TODO())
 	assert.Equal(t, fmt.Sprintf("invalid value: invalid resource name %q: length must be no more than 25 characters: name: spec.additionalPACControllers", "testlengthwhichexceedsthemaximumlength"), err.Error())
+}
+
+func TestValidateAddtionalPACControllerInvalidSetting(t *testing.T) {
+	opacCR := &OpenShiftPipelinesAsCode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "name",
+			Namespace: "namespace",
+		},
+		Spec: OpenShiftPipelinesAsCodeSpec{
+			CommonSpec: CommonSpec{
+				TargetNamespace: "Openshift-Pipelines",
+			},
+			PACSettings: PACSettings{
+				Settings: map[string]string{},
+				AdditionalPACControllers: map[string]AdditionalPACControllerConfig{
+					"testlength": {
+						ConfigMapName: "test-configmap",
+						SecretName:    "test-secret",
+						Settings: map[string]string{
+							"custom-console-url": "test/path",
+							"application-name":   "Additional PACController CI",
+						},
+					},
+				},
+			},
+		},
+	}
+	err := opacCR.Validate(context.TODO())
+	assert.Equal(t, "invalid value: invalid value: invalid value for URL, error: parse \"test/path\": invalid URI for request: validation failed for field custom-console-url: spec.additionalPACControllers.settings", err.Error())
 }
