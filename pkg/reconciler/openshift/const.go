44d099008deae22b66c92181cb974ea1bc316fa3
Jawed khelil <jkhelil@redhat.com>
2024-09-26 10:15:56 +0200

    Revert "make rbac max concurrent calls configurable via env variable"
    This reverts commit b45d19749e4e96dbfe31d706525adfc0cfcce1f1.

--- pkg/reconciler/openshift/const.go
+++ pkg/reconciler/openshift/const.go
@@ -5,8 +5,4 @@ const (
 	OperandOpenShiftPipelineAsCode  = "openshift-pipeline-as-code"
 	// NamespaceSCCAnnotation is used to set SCC for a given namespace
 	NamespaceSCCAnnotation = "operator.tekton.dev/scc"
-
-	// RbacProvisioningMaxConcurrentCalls is used to set a go routine pool size when
-	// we reconcile namespaces and do rbac provisonning
-	RbacProvisioningMaxConcurrentCalls = "OCP_RBAC_MAX_CONCURRENT_CALLS"
 )
