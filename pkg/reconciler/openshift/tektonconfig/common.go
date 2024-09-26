00c672f777df672523760dee4a4a7f99839f441b
Jawed khelil <jkhelil@redhat.com>
2024-09-26 10:16:38 +0200

    Revert "Run rbac createresources for namespaces concurrently to avoid slowness"
    This reverts commit f753a7988b255f7a36611cf0b285c81ab49a02d1.

--- pkg/reconciler/openshift/tektonconfig/common.go
+++ pkg/reconciler/openshift/tektonconfig/common.go
@@ -97,7 +97,8 @@ func deleteInstallerSet(ctx context.Context, oc versioned.Interface, tc *v1alpha
 // checkIfInstallerSetExist checks if installer set exists for a component and return true/false based on it
 // and if installer set which already exist is of older version then it deletes and return false to create a new
 // installer set
-func checkIfInstallerSetExist(ctx context.Context, oc versioned.Interface, relVersion string) (*v1alpha1.TektonInstallerSet, error) {
+func checkIfInstallerSetExist(ctx context.Context, oc versioned.Interface, relVersion string,
+	tc *v1alpha1.TektonConfig) (*v1alpha1.TektonInstallerSet, error) {
 
 	labelSelector, err := common.LabelSelector(rbacInstallerSetSelector)
 	if err != nil {
