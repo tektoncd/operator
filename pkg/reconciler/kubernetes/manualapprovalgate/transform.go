a18aaf97d600fb52123e6d4fefa854026cd426e8
Jawed khelil <jkhelil@redhat.com>
2025-03-14 15:30:40 +0100

    inject environment variable KUBERNETES_MIN_VERSION

--- pkg/reconciler/kubernetes/manualapprovalgate/transform.go
+++ pkg/reconciler/kubernetes/manualapprovalgate/transform.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The Tekton Authors
+Copyright 2024 The Tekton Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tektondashboard
+package manualapprovalgate
 
 import (
 	"context"
@@ -25,32 +25,25 @@ import (
 	"github.com/tektoncd/operator/pkg/reconciler/kubernetes/tektoninstallerset/client"
 )
 
-const (
-	externalLogsArg = "--external-logs="
-)
-
 func filterAndTransform(extension common.Extension) client.FilterAndTransform {
 	return func(ctx context.Context, manifest *mf.Manifest, comp v1alpha1.TektonComponent) (*mf.Manifest, error) {
-		dashboard := comp.(*v1alpha1.TektonDashboard)
-		trns := extension.Transformers(dashboard)
+		magCR := comp.(*v1alpha1.ManualApprovalGate)
+		magImages := common.ToLowerCaseKeys(common.ImagesFromEnv(common.ManualApprovalGatePrefix))
 		extra := []mf.Transformer{
-			common.InjectOperandNameLabelOverwriteExisting(v1alpha1.OperandTektoncdDashboard),
-			common.AddConfiguration(dashboard.Spec.Config),
-			common.AddDeploymentRestrictedPSA(),
+			common.InjectOperandNameLabelOverwriteExisting(v1alpha1.ManualApprovalGates),
+			common.DeploymentImages(magImages),
 			common.DeploymentEnvVarKubernetesMinVersion(),
+			common.AddDeploymentRestrictedPSA(),
 		}
-		trns = append(trns, extra...)
-		if dashboard.Spec.ExternalLogs != "" {
-			updatedExternalLogsArg := externalLogsArg + dashboard.Spec.ExternalLogs
-			trns = append(trns, common.ReplaceDeploymentArg("tekton-dashboard", externalLogsArg, updatedExternalLogsArg))
-		}
-		if err := common.Transform(ctx, manifest, dashboard, trns...); err != nil {
+		extra = append(extra, extension.Transformers(magCR)...)
+		err := common.Transform(ctx, manifest, magCR, extra...)
+		if err != nil {
 			return &mf.Manifest{}, err
 		}
 
 		// additional options transformer
 		// always execute as last transformer, so that the values in options will be final update values on the manifests
-		if err := common.ExecuteAdditionalOptionsTransformer(ctx, manifest, dashboard.Spec.GetTargetNamespace(), dashboard.Spec.Dashboard.Options); err != nil {
+		if err := common.ExecuteAdditionalOptionsTransformer(ctx, manifest, magCR.Spec.GetTargetNamespace(), magCR.Spec.Options); err != nil {
 			return &mf.Manifest{}, err
 		}
 
