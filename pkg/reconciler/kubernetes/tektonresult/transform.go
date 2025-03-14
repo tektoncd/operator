de929af7cd3f1fba6f61c0503825b4e47100c91a
Jawed khelil <jkhelil@redhat.com>
2025-03-14 15:30:40 +0100

    inject environment variable KUBERNETES_MIN_VERSION

--- pkg/reconciler/kubernetes/tektonresult/transform.go
+++ pkg/reconciler/kubernetes/tektonresult/transform.go
@@ -95,6 +95,7 @@ func (r *Reconciler) transform(ctx context.Context, manifest *mf.Manifest, comp 
 		common.AddDeploymentRestrictedPSA(),
 		common.AddStatefulSetRestrictedPSA(),
 		common.DeploymentImages(resultImgs),
+		common.DeploymentEnvVarKubernetesMinVersion(),
 		common.StatefulSetImages(resultImgs),
 	}
 	extra = append(extra, r.extension.Transformers(instance)...)
