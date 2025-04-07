9264968e28051f31d8c864be4ea8e4495d47a71c
Jawed khelil <jkhelil@redhat.com>
2025-04-07 17:37:14 +0200

    Improve operator logging for pipeline reconcile operations

--- pkg/reconciler/kubernetes/tektonpipeline/reconcile.go
+++ pkg/reconciler/kubernetes/tektonpipeline/reconcile.go
@@ -26,6 +26,7 @@ import (
 	"github.com/tektoncd/operator/pkg/reconciler/common"
 	"github.com/tektoncd/operator/pkg/reconciler/kubernetes/tektoninstallerset/client"
 	"k8s.io/client-go/kubernetes"
+	"knative.dev/pkg/apis"
 	"knative.dev/pkg/logging"
 	pkgreconciler "knative.dev/pkg/reconciler"
 )
@@ -57,16 +58,22 @@ var _ tektonpipelinereconciler.Interface = (*Reconciler)(nil)
 // ReconcileKind compares the actual state with the desired, and attempts to
 // converge the two.
 func (r *Reconciler) ReconcileKind(ctx context.Context, tp *v1alpha1.TektonPipeline) pkgreconciler.Event {
-	logger := logging.FromContext(ctx).With("name", tp.GetName())
+	logger := logging.FromContext(ctx).With(
+		"name", tp.GetName(),
+		"namespace", tp.GetNamespace(),
+		"resourceVersion", tp.GetResourceVersion(),
+	)
+	logger.Debugw("Starting TektonPipeline reconciliation",
+		"version", r.pipelineVersion,
+		"status", tp.Status.GetCondition(apis.ConditionReady))
+
 	tp.Status.InitializeConditions()
 	tp.Status.SetVersion(r.pipelineVersion)
 
 	if tp.GetName() != v1alpha1.PipelineResourceName {
-		msg := fmt.Sprintf("Resource ignored, Expected Name: %s, Got Name: %s",
-			v1alpha1.PipelineResourceName,
-			tp.GetName(),
-		)
-		logger.Error(msg)
+		msg := fmt.Sprintf("Resource ignored: expected name '%s', got '%s'",
+			v1alpha1.PipelineResourceName, tp.GetName())
+		logger.Errorw("Invalid resource name", "expectedName", v1alpha1.PipelineResourceName, "actualName", tp.GetName())
 		tp.Status.MarkNotReady(msg)
 		return nil
 	}
@@ -75,33 +82,41 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, tp *v1alpha1.TektonPipel
 	tp.SetDefaults(ctx)
 
 	// reconcile target namespace
+	logger.Debug("Reconciling target namespace")
 	if err := common.ReconcileTargetNamespace(ctx, nil, nil, tp, r.kubeClientSet); err != nil {
+		logger.Errorw("Failed to reconcile target namespace", "error", err)
 		return err
 	}
 
 	if err := r.installerSetClient.RemoveObsoleteSets(ctx); err != nil {
-		logger.Error("failed to remove obsolete installer sets: %v", err)
+		logger.Errorw("Failed to remove obsolete installer sets", "error", err)
 		return err
 	}
+	logger.Debug("Obsolete installer sets removed")
 
 	// Pipeline controller is deployed as statefulset, ensure deployment installerset is deleted
 	if tp.Spec.Performance.StatefulsetOrdinals != nil && *tp.Spec.Performance.StatefulsetOrdinals {
+		logger.Debugw("Cleaning up deployment installer set", "usingStatefulset", true)
 		if err := r.installerSetClient.CleanupSubTypeDeployment(ctx); err != nil {
-			logger.Error("failed to delete main deployment installer set: %v", err)
+			logger.Errorw("Failed to delete main deployment installer set", "error", err)
 			return err
 		}
+		logger.Debug("Deployment installer set deleted")
 	} else {
 		// Pipeline controller is deployed as deployment, ensure statefulset installerset is deleted
 		if err := r.installerSetClient.CleanupSubTypeStatefulset(ctx); err != nil {
-			logger.Error("failed to delete main statefulset installer set: %v", err)
+			logger.Debugw("Cleaning up statefulset installer set", "usingDeployment", true)
 			return err
 		}
+		logger.Debug("Statefulset installer set deleted")
 	}
 
+	logger.Debug("Executing pre-reconciliation")
 	if err := r.extension.PreReconcile(ctx, tp); err != nil {
 		msg := fmt.Sprintf("PreReconciliation failed: %s", err.Error())
-		logger.Error(msg)
+		logger.Errorw("Pre-reconciliation failed", "error", err)
 		if err == v1alpha1.REQUEUE_EVENT_AFTER {
+			logger.Infow("Pre-reconciliation requested requeue")
 			return err
 		}
 		tp.Status.MarkPreReconcilerFailed(msg)
@@ -109,6 +124,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, tp *v1alpha1.TektonPipel
 	}
 
 	// Mark PreReconcile Complete
+	logger.Info("Pre-reconciliation completed successfully")
 	tp.Status.MarkPreReconcilerComplete()
 
 	// When TektonPipeline component is deleted targetNamespace was getting deleted,
@@ -116,30 +132,36 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, tp *v1alpha1.TektonPipel
 	// in turn also updated the ownerRef of targetNamespace from TektonConfig to TektonPipeline.
 	// Since namespace is created in TektonConfig reconciler hence deleting TektonPipeline
 	// component should not delete the targetNamespace hence filtering out the namespace here
+	logger.Debug("Filtering out namespace from manifest")
 	manifest := r.manifest.Filter(mf.Not(mf.ByKind("Namespace")))
 
 	// Ensure webhook deadlock prevention before applying the manifest
+	logger.Debug("Preempting webhook deadlock")
 	if err := common.PreemptDeadlock(ctx, &manifest, r.kubeClientSet, v1alpha1.PipelineResourceName); err != nil {
-		msg := fmt.Sprintf("Failed to preempt webhook deadlock: %s", err.Error())
-		logger.Error(msg)
+		logger.Errorw("Failed to preempt webhook deadlock", "error", err)
 		return err
 	}
 
 	//Apply manifest
+	logger.Debug("Applying main manifest")
 	if err := r.installerSetClient.MainSet(ctx, tp, &manifest, filterAndTransform(r.extension)); err != nil {
 		msg := fmt.Sprintf("Main Reconcilation failed: %s", err.Error())
-		logger.Error(msg)
+		logger.Errorw("Failed to apply main installer set", "error", err)
 		if err == v1alpha1.REQUEUE_EVENT_AFTER {
+			logger.Infow("Main reconciliation requested requeue")
 			return err
 		}
 		tp.Status.MarkInstallerSetNotReady(msg)
 		return nil
 	}
+	logger.Info("Main manifest applied successfully")
 
+	logger.Debug("Executing post-reconciliation")
 	if err := r.extension.PostReconcile(ctx, tp); err != nil {
 		msg := fmt.Sprintf("PostReconciliation failed: %s", err.Error())
-		logger.Error(msg)
+		logger.Errorw("Post-reconciliation failed", "error", err)
 		if err == v1alpha1.REQUEUE_EVENT_AFTER {
+			logger.Info("Post-reconciliation requested requeue")
 			return err
 		}
 		tp.Status.MarkPostReconcilerFailed(msg)
@@ -147,7 +169,9 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, tp *v1alpha1.TektonPipel
 	}
 
 	// Mark PostReconcile Complete
+	logger.Info("Post-reconciliation completed successfully")
 	tp.Status.MarkPostReconcilerComplete()
 
+	logger.Info("TektonPipeline reconciliation completed successfully")
 	return nil
 }
