7d5f276c3f03827f12bd61b25d0a0bbee7a6cc2e
Pramod Bindal <prbindal@redhat.com>
2025-07-08 17:05:31 +0530

    Enable TektonPruner Configuration from tektonconfig
    Signed-off-by: Pramod Bindal <prbindal@redhat.com>

--- pkg/reconciler/kubernetes/tektonpruner/controller.go
+++ pkg/reconciler/kubernetes/tektonpruner/controller.go
@@ -63,12 +63,14 @@ func NewExtendedController(generator common.ExtensionGenerator) injection.Contro
 		tisClient := operatorclient.Get(ctx).OperatorV1alpha1().TektonInstallerSets()
 		metrics, _ := NewRecorder()
 		c := &Reconciler{
+			operatorClientSet:  operatorclient.Get(ctx),
 			kubeClientSet:      kubeclient.Get(ctx),
 			pipelineInformer:   tektonPipelineinformer.Get(ctx),
 			installerSetClient: client.NewInstallerSetClient(tisClient, operatorVer, prunerVer, v1alpha1.KindTektonPruner, metrics),
 			extension:          generator(ctx),
 			manifest:           manifest,
 			prunerVersion:      prunerVer,
+			operatorVersion:    operatorVer,
 		}
 		impl := tektonPrunerreconciler.NewImpl(ctx, c)
 
