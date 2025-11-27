c92fbc010d9d8d97b8df64f5f8e12dc6b9dfc298
shverma <shverma@redhat.com>
2025-11-27 17:07:13 +0530

    Auto upgrade Hub PostgreSQL 13 to 15
    - This patch adds a wrapper script which checks the
     version of the running DB directory structure and
     trigger a one time upgrade if required.
    - Fixed a bug in the Hub API Transformer method
    
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- pkg/reconciler/openshift/tektonhub/extension_test.go
+++ pkg/reconciler/openshift/tektonhub/extension_test.go
@@ -43,9 +43,64 @@ func TestUpdateDbDeployment(t *testing.T) {
 	env := d.Spec.Template.Spec.Containers[0].Env
 	assert.Equal(t, env[0].Name, "POSTGRESQL_DATABASE")
 
+	// Verify PGDATA environment variable value is updated
+	var pgdataValue string
+	for _, e := range env {
+		if e.Name == "PGDATA" {
+			pgdataValue = e.Value
+			break
+		}
+	}
+	assert.Equal(t, pgdataValue, "/var/lib/pgsql/data")
+
 	mountPath := d.Spec.Template.Spec.Containers[0].VolumeMounts[0].MountPath
 	assert.Equal(t, mountPath, "/var/lib/pgsql/data")
 
 	cmd := d.Spec.Template.Spec.Containers[0].ReadinessProbe.Exec.Command
 	assert.Equal(t, strings.Contains(cmd[2], "POSTGRESQL_USER"), true)
+}
+
+func TestInjectPostgresUpgradeSupport(t *testing.T) {
+	testData := path.Join("testdata", "update-db-deployment.yaml")
+
+	manifest, err := mf.ManifestFrom(mf.Recursive(testData))
+	assert.NilError(t, err)
+
+	newManifest, err := manifest.Transform(injectPostgresUpgradeSupport())
+	assert.NilError(t, err)
+
+	d := &appsv1.Deployment{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(newManifest.Resources()[0].Object, d)
+	assert.NilError(t, err)
+
+	// Verify command is set to use the wrapper script
+	command := d.Spec.Template.Spec.Containers[0].Command
+	assert.Equal(t, len(command), 2)
+	assert.Equal(t, command[0], "/bin/bash")
+	assert.Equal(t, command[1], "/upgrade-scripts/postgres-wrapper.sh")
+
+	// Verify volume mount for upgrade scripts is added
+	volumeMounts := d.Spec.Template.Spec.Containers[0].VolumeMounts
+	var upgradeScriptsMountFound bool
+	for _, vm := range volumeMounts {
+		if vm.Name == "upgrade-scripts" {
+			upgradeScriptsMountFound = true
+			assert.Equal(t, vm.MountPath, "/upgrade-scripts")
+			break
+		}
+	}
+	assert.Equal(t, upgradeScriptsMountFound, true)
+
+	// Verify volume for upgrade scripts ConfigMap is added
+	volumes := d.Spec.Template.Spec.Volumes
+	var upgradeScriptsVolumeFound bool
+	for _, vol := range volumes {
+		if vol.Name == "upgrade-scripts" {
+			upgradeScriptsVolumeFound = true
+			assert.Equal(t, vol.ConfigMap.Name, "tekton-hub-postgres-upgrade-scripts")
+			assert.Equal(t, *vol.ConfigMap.DefaultMode, int32(0755))
+			break
+		}
+	}
+	assert.Equal(t, upgradeScriptsVolumeFound, true)
 }
