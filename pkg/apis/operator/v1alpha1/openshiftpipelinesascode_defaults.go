d55a761d1cbbb5df0660e6b2f7251c8f27a16aae
shverma <shverma@redhat.com>
2025-12-08 18:39:29 +0530

    Override the catalog URL when the Hub catalog type is set to artifacthub
    When a custom Hub catalog is configured, PAC’s syncConfig incorrectly forces
    the catalog type to artifacthub and overrides the Hub URL with https://artifacthub.io.
    This fix ensures the URL is overridden only when the catalog type is explicitly set to artifacthub
    
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- pkg/apis/operator/v1alpha1/openshiftpipelinesascode_defaults.go
+++ pkg/apis/operator/v1alpha1/openshiftpipelinesascode_defaults.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 	"sync"
 
+	hubtypes "github.com/openshift-pipelines/pipelines-as-code/pkg/hub/vars"
 	pacSettings "github.com/openshift-pipelines/pipelines-as-code/pkg/params/settings"
 	"go.uber.org/zap"
 	"knative.dev/pkg/logging"
@@ -57,7 +58,9 @@ func (set *PACSettings) setPACDefaults(logger *zap.SugaredLogger) {
 	// Override the default ArtifactHub URL to use https://artifacthub.io instead of https://artifacthub.io/api/v1
 	if defaultCatalog, ok := defaultPacSettings.HubCatalogs.Load("default"); ok {
 		catalog := defaultCatalog.(pacSettings.HubCatalog)
-		catalog.URL = "https://artifacthub.io"
+		if catalog.Type == hubtypes.ArtifactHubType {
+			catalog.URL = "https://artifacthub.io"
+		}
 		defaultPacSettings.HubCatalogs.Store("default", catalog)
 	}
 
