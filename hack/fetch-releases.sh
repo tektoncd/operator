b4d24e0ed61e6286bdf32b0e8e80ad226fba41e3
mbpavan <pbheeman@redhat.com>
2026-04-15 09:43:11 +0530

    resolve openshift-pipelines/pipelines-as-code to tektoncd repo

--- hack/fetch-releases.sh
+++ hack/fetch-releases.sh
@@ -210,9 +210,9 @@ release_yaml_pac() {
     dirPath=${comp_dir}/${version}
 
     if [[ ${version} == "stable" ||  ${version} == "nightly" ]]; then
-      url="https://raw.githubusercontent.com/openshift-pipelines/pipelines-as-code/${version}/release.yaml"
+      url="https://raw.githubusercontent.com/tektoncd/pipelines-as-code/${version}/release.yaml"
     else
-      url="https://raw.githubusercontent.com/openshift-pipelines/pipelines-as-code/release-${version}/release.yaml"
+      url="https://raw.githubusercontent.com/tektoncd/pipelines-as-code/release-${version}/release.yaml"
     fi
 
     dest=${dirPath}/${fileName}.yaml
@@ -247,7 +247,7 @@ release_yaml_pac() {
     do
       echo "fetching PipelineRun template for runtime: $run"
 
-      source="https://raw.githubusercontent.com/openshift-pipelines/pipelines-as-code/${version}/pkg/cmd/tknpac/generate/templates/${run}.yaml"
+      source="https://raw.githubusercontent.com/tektoncd/pipelines-as-code/${version}/pkg/cmd/tknpac/generate/templates/${run}.yaml"
       dest_dir="${ko_data}/tekton-addon/pipelines-as-code-templates"
       mkdir -p ${dest_dir} || true
       destination="${dest_dir}/${run}.yaml"
